4a4bfe95ec9e78a722ed3821f72ace498d1f75f2
Manyu Sharma <manyusharma96@gmail.com>
2017-06-21 14:22:09 -0400

    Adding code for receiving end

---
+++ networkingDemo/server.py
@@ -0,0 +1,17 @@
+import socket
+import time
+import os 
+import random
+
+class Server():
+	def __init__(self,Adress=('',5000),MaxClient=1):
+    	self.s = socket.socket()
+    	self.s.bind(Adress)
+    	self.s.listen(MaxClient)
+ 	def WaitForConnection(self):
+    	self.Client, self.Adr=(self.s.accept())
+    	print('Got a connection from: '+str(self.Client)+'.')
+
+
+s = Server()
+s.WaitForConnection()
