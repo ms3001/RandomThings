649e9417f958554bdc172388065b074fd37ca9f7
Manyu Sharma <manyusharma96@gmail.com>
2017-09-07 22:07:08 -0400

    made a few changes to visualizing code.

--- TwitchProject/Visualizer.m
+++ TwitchProject/Visualizer.m
@@ -2,19 +2,23 @@ data = [];
 path = 'chatoutput';
 
 % Loading data
-for i = 0:14
+for i = 64:85
     temp = dlmread(strcat(path,num2str(i),'.txt'),'\t');
     data = [data; temp];
 end
 
 freq = zeros(int32(max(data)) + 1,1);
 
-
+% Generating activity graph
 for i = 1:(max(data))
-    i
     temp = find(abs(data - i) < 20);
     count = length(temp);
     freq(i) = count;
 end
 
 plot(freq)
+hold on;
+plot(mean(freq)*ones(length(freq),1))
+plot(tsmovavg(freq,'s',600,1))
+plot(smooth(freq,600)*1.8)
+% Finding peaks
